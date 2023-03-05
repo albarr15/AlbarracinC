b95aea7a83cd4558dffc8013f0898fbec0782dc6
albarr15 <cmalbarracin15@gmail.com>
2023-03-05 19:16:25 +0800

    Fixed structure of program and finished function transitions

--- AlbarracinC.c
+++ AlbarracinC.c
@@ -5,8 +5,19 @@
 
 #define ENTER 13
 
+// function declarations
+void displayMenu();
+void displaymanageData();
+void addRecords();
+void editRecords();
+void deleteRecords();
+void importData();
+void exportData();
 void manageData();
 void askPassword(int isValidPW, int *ptr_isValidPW);
+void playQuiz();
+void viewScores();
+void Play();
 
 /*
  NOTES TO SELF:
@@ -14,9 +25,6 @@ void askPassword(int isValidPW, int *ptr_isValidPW);
  - Try out all possible routes of the program to recheck inappropriate exits
  - Add function descriptions when finished
  */
-
-
-
 
 
 
@@ -33,6 +41,143 @@ displayMenu()
     printf("[2] Play\n");
     printf("[3] Exit\n");
 }
+
+/* displaymanageData shows the Graphic User Interface for the options in the Manage Data section of the program
+    @param <none>
+    @return <none>
+    Pre-condition: User has selected Manage Data from the Main Menu
+*/
+void
+displaymanageData()
+{
+    printf("[1] Add a record\n");
+    printf("[2] Edit a record\n");
+    printf("[3] Delete a record\n");
+    printf("[4] Import data\n");
+    printf("[5] Export data\n");
+    printf("[6] Go back to MAIN MENU\n");
+}
+
+/* manageData allows the admin to add, edit, delete records, as well as import and export data after inputting the correct admin password
+    @param <none>
+    @return <none>
+    Pre-condition: User has selected Manage Data from the Main Menu and has not yet opted to go back to Main Menu
+*/
+void
+manageData()
+{
+	int isValidPW = 0;
+    int nInput;
+    int bQuit = 0;
+	
+    // ask for the admin password
+    askPassword(isValidPW, &isValidPW);
+    
+    // if invalid password
+    if (isValidPW)
+    {
+        while (bQuit == 0)
+        {
+            printf("Managing Data ...\n");
+            
+            displaymanageData();
+            scanf("%d", &nInput);
+            
+            switch (nInput)
+            {
+                case 1:
+                    addRecords();
+                    break;
+                    
+                case 2:
+                    editRecords();
+                    break;
+                    
+                case 3:
+                    deleteRecords();
+                    break;
+                    
+                case 4:
+                    importData();
+                    break;
+                    
+                case 5:
+                    exportData();
+                    break;
+                    
+                case 6:
+                    printf("Going back to MAIN MENU...\n");
+                    bQuit = 1;
+                    break;
+                    
+                default:
+                    printf("Please select one of the options provided.\n");
+                    break;
+            }
+        }
+    }
+}
+
+/*
+    askPassword requires the admin to user the correct admin password as well as masks the password with asterisks while it is being typed for additional security
+    @param isValidPW is an integer variable where the validity of the password input is stored
+        *ptr_isValidPW is a pointer to the variable 'isValidPW' in order for the manageData to check the validity of the password
+    @return <none>
+    Pre-condition: User has selected Manage Data from the menu, user cannot backspace or add spaces when entering the password
+*/
+void askPassword(int isValidPW, int *ptr_isValidPW)
+{
+    const char sADMINPW[] = "Genquiz411";
+    char sPWInput[11];
+    char ch;
+    int i = 0;
+    int n = 0;
+    
+    printf("Please input admin password: ");
+    
+    /*
+    // Mask password with asterisks
+    // if character entered is NOT the enter key
+    while ((ch = _getch()) != 13)
+    {
+        sPWInput[i] = ch;
+        printf("*");
+        i++;
+    }
+    
+    // add null byte to specify end of string
+    sPWInput[i] = '\0';
+    printf("\n");
+     */
+    
+    // use only when editing in MacOS
+    scanf("%s", sPWInput);
+    
+    // if password is correct
+    if (strcmp(sPWInput, sADMINPW) == 0)
+    {
+        // set isValidPW to 1
+        *ptr_isValidPW = 1;
+        printf("Accessing admin controls...\n");
+    }
+    // if input is 1,
+    else if (strcmp(sPWInput, "1") == 0)
+    {
+        // go back to main menu
+        printf("Going back...\n");
+        // set isValidPW to 0
+        *ptr_isValidPW = 0;
+    }
+    // if incorrect password
+    else if (strcmp(sPWInput, sADMINPW) != 0)
+    {
+        printf("Incorrect Password. Try again.\n");
+        printf("[1] Go back to Main Menu\n\n");
+        // re-attempt to ask password
+        askPassword(isValidPW, &isValidPW);
+    }
+}
+
 
 void addRecords()
 {
@@ -59,145 +204,11 @@ void exportData()
     printf("Exporting data...\n");
 }
 
-
-
-/* manageData allows the admin to add, edit, delete records, as well as import and export data after inputting the correct admin password
+/* Play allows the user to play the quiz, view the scores, as well as go back to the Main Menu
     @param <none>
     @return <none>
-    Pre-condition: User has opened the program
+    Pre-condition: User has selected Play from the Main Menu and has not yet opted to go back to Main Menu
 */
-void
-manageData()
-{
-	int isValidPW = 0;
-    int nInput;
-    int bQuit = 0;
-	
-    // ask for the admin password
-    askPassword(isValidPW, &isValidPW);
-    
-    if (!isValidPW)
-    {
-        bQuit = 1;
-	}
-    else
-    {
-        printf("Managing Data ...\n");
-        
-        // display manageData options
-        printf("[1] Add a record\n");
-        printf("[2] Edit a record\n");
-        printf("[3] Delete a record\n");
-        printf("[4] Import data\n");
-        printf("[5] Export data\n");
-        printf("[6] Go back to MAIN MENU\n");
-        
-        scanf("%d", &nInput);
-        while (bQuit == 0)
-        {
-            switch (nInput)
-            {
-                case 1:
-                    addRecords();
-                    break;
-                    
-                case 2:
-                    editRecords();
-                    break;
-                    
-                case 3:
-                    deleteRecords();
-                    break;
-                    
-                case 4:
-                    importData();
-                    break;
-                    
-                case 5:
-                    exportData();
-                    break;
-                    
-                case 6:
-                    printf("Going back to MAIN MENU...\n");
-                    break;
-                    
-                default:
-                    printf("Please select one of the options provided.\n");
-                    break;
-            }
-            bQuit = 1;
-        }
-    }
-}
-
-/*
-	askPassword requires the admin to user the correct admin password as well as masks the password with asterisks while it is being typed for additional security
-	@param isValidPW is an integer variable where the validity of the password input is stored
-		*ptr_isValidPW is a pointer to the variable 'isValidPW' in order for the manageData to check the validity of the password
-	@return <none>
-	Pre-condition: User has selected Manage Data from the menu, user cannot backspace or add spaces when entering the password
-*/
-void
-askPassword(int isValidPW, int *ptr_isValidPW)
-{
-    const char sADMINPW[] = "Genquiz411";
-    char sPWInput[11];
-    char ch;
-    int i = 0;
-    int n = 0;
-    
-    printf("Please input admin password: ");
-    
-    /*
-    // Mask password with asterisks
-    // if character entered is NOT the enter key
-    while ((ch = _getch()) != 13)
-    {
-		sPWInput[i] = ch;
-    	printf("*");
-    	i++;
-	}
-	
-	// add null byte to specify end of string
-    sPWInput[i] = '\0';
-    printf("\n");
-     */
-    
-    // use only when editing in MacOS
-    scanf("%s", sPWInput);
-    
-    // if password is valid
-    if (strcmp(sPWInput, sADMINPW) == 0)
-    {
-    	n = 1;
-    	*ptr_isValidPW = n;
-        printf("Accessing admin controls...\n");
-    }
-    // if input is 1, go back to main menu
-    else if (strcmp(sPWInput, "1") == 0)
-    {
-        printf("Going back...\n");
-        *ptr_isValidPW = 0;
-    }
-    // if incorrect password
-    else if (strcmp(sPWInput, sADMINPW) != 0)
-    {
-        printf("Incorrect Password. Try again.\n");
-        printf("[1] Go back to Main Menu\n\n");
-        askPassword(isValidPW, &isValidPW);
-    }
-}
-
-void playQuiz()
-{
-    printf("Playing quiz...\n");
-}
-
-void viewScores()
-{
-    printf("Viewing scores...\n");
-}
-
 void Play()
 {
     int bQuit = 0;
@@ -234,6 +245,16 @@ void Play()
         }
         bQuit = 1;
     }
+}
+
+void playQuiz()
+{
+    printf("Playing quiz...\n");
+}
+
+void viewScores()
+{
+    printf("Viewing scores...\n");
 }
 
 int main()
